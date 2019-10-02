9c97ac480fbc3231513559b7b7c5703cff576a58
Przemysław Dziągwa <przemyslaw.dziagwa@gmail.com>
2019-10-03 00:39:09 +0200

    gotowy wstępny leyout gry i dziwaczny toggle

--- js/App.js
+++ js/App.js
@@ -1,13 +1,47 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import Dodawanie from "./Dodawanie";
+
+
+
+
+class Game extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            odejmowanie : false,
+            dodawanie : false,
+            mnożenie : false,
+            dzielenie : false
+        }
+    }
+    render() {
+        return(
+            <>
+                {this.state.odejmowanie === false && this.state.dodawanie === false && this.state.mnożenie === false && this.state.dzielenie === false ?
+                <div>
+                    <button onClick={() => {this.setState({odejmowanie : true})}}>doejmowanie</button>
+                    <button onClick={() => {this.setState({dodawanie : true})}}>dodawanie</button>
+                    <button onClick={() => {this.setState({mnożenie : true})}}>mnożenie</button>
+                    <button onClick={() => {this.setState({dzielenie : true})}}>dzielenie</button>
+                </div> : null}
+                {this.state.dodawanie === true ? <Dodawanie/> : null}
+
+            </>
+        )
+    }
+}
+
 
 class App extends React.Component{
     render() {
         return(
             <>
-                <h1>cześ</h1>
+                <Game elo={'no elo'}/>
             </>
         )
     }
 }
+
+
 ReactDOM.render(<App/>, document.getElementById("app"));
